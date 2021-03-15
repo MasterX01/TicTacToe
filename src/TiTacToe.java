9171323bb395211285e5671441e9185477f56375
MasterX01 <aakashsaxena14@gmail.com>
2021-03-15 09:01:31 +0530

    [Akash] ADD. added UC-1 working code

--- src/TiTacToe.java
+++ src/TiTacToe.java
@@ -1,5 +1,9 @@
 public class TiTacToe {
+    // UC-1
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe Program");
+        char[] board = new char[10];
+        for(int i = 1; i < board.length; i++) {
+            board[i] = (char)' ';
+        }
     }
 }
