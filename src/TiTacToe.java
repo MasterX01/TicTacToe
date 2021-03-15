0b630e17cbb73a7e30c0b689cb7a050f4d682dc4
MasterX01 <aakashsaxena14@gmail.com>
2021-03-15 09:42:25 +0530

    [Akash] ADD. addded UC-2 working code

--- src/TiTacToe.java
+++ src/TiTacToe.java
@@ -1,14 +1,38 @@
+import java.util.Scanner;
+
 public class TiTacToe {
     // UC-1
-    char[] board = new char[10];
-    public void makeEmpty() {
+    static char[] board = new char[10];
+    public static char player;
+    public static char computer;
+
+    public static void makeEmpty() {
         for (int i = 1; i < board.length; i++) {
-            board[i] = (char) ' ';
+            board[i] = ' ';
         }
     }
 
-    public static void main(String[] args){
-        TiTacToe obj = new TiTacToe();
-        obj.makeEmpty();
+    public static void playerSelect() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the Symbol you choose to with (X/O): ");
+        String choice = scan.next();
+        switch(choice) {
+            case "X":
+                player = 'X';
+                computer = 'O';
+                break;
+            case "O":
+                player = 'O';
+                computer = 'X';
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        makeEmpty();
+        playerSelect();
     }
 }
