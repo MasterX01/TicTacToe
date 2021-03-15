d1b6b3e7ae986ab877ca95b1121d49a5139573d7
MasterX01 <aakashsaxena14@gmail.com>
2021-03-15 09:21:16 +0530

    [Akash] REFACTOR. made changes suggested

--- src/TiTacToe.java
+++ src/TiTacToe.java
@@ -1,14 +1,13 @@
 public class TiTacToe {
     // UC-1
     char[] board = new char[10];
-    public void makeEmpty() {
+    public static void makeEmpty() {
         for (int i = 1; i < board.length; i++) {
-            board[i] = (char) ' ';
+            board[i] = ' ';
         }
     }
 
     public static void main(String[] args){
-        TiTacToe obj = new TiTacToe();
-        obj.makeEmpty();
+        makeEmpty();
     }
 }
